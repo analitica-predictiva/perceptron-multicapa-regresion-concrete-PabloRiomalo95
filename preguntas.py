b44d1b028962499b52fc01602963ce32c29b7084
PabloRiomalo95 <111611986+PabloRiomalo95@users.noreply.github.com>
2022-12-02 16:47:06 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -16,45 +16,45 @@ def pregunta_01():
     Carga y separación de los datos en `X` `y`
     """
     # Lea el archivo `concrete.csv` y asignelo al DataFrame `df`
-    df = ____  
-
+    df = pd.read_csv('concrete.csv',sep = ',',thousands = None,decimal = '.')
+    
     # Asigne la columna `strength` a la variable `y`.
-    ____ = ____  
-
+    y = df['strength']
+    
     # Asigne una copia del dataframe `df` a la variable `X`.
-    ____ = ____.____(____)  
-
+    x = df.copy()  
+    
     # Remueva la columna `strength` del DataFrame `X`.
-    ____.____(____)  
+    x.drop(['strength'],axis=1,inplace=True)  
 
     # Retorne `X` y `y`
     return x, y
 
 
 def pregunta_02():
+
     """
     Preparación del dataset.
     """
-
+    
     # Importe train_test_split
-    from ____ import ____
-
+    from sklearn.model_selection import train_test_split
+    
     # Cargue los datos de ejemplo y asigne los resultados a `X` y `y`.
     x, y = pregunta_01()
-
+    
     # Divida los datos de entrenamiento y prueba. La semilla del generador de números
     # aleatorios es 12453. Use el 75% de los patrones para entrenamiento.
-    (  
-        x_train,  
+    (  x_train,  
         x_test,  
         y_train,  
         y_test,  
-    ) = ____(  
-        ____,  
-        ____,  
-        test_size=____,  
-        random_state=____,  
-    )  
+    ) = train_test_split(  
+        x,  
+        y,  
+        test_size=0.25,  
+        random_state=12453,  
+    )
 
     # Retorne `X_train`, `X_test`, `y_train` y `y_test`
     return x_train, x_test, y_train, y_test
